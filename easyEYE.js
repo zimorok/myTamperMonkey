8b6686061cb15b15975d873c3a0d767b69dd96c1
zimorok <xsqade@gmail.com>
2020-05-25 13:25:48 +0800

    Update easyEYE.js
    fixed CSS button group positioning

--- easyEYE.js
+++ easyEYE.js
@@ -2,14 +2,14 @@
 // @name         easyEye
 // @namespace    http://tampermonkey.net/
 // @updateURL	 https://github.com/zimorok/myTamperMonkey/raw/master/easyEYE.js
-// @version      0.1.4
+// @version      0.1.5
 // @description  Read your web novel easily on mobile with paginated content
 // @author       Zimorok
 // @match        *://boxnovel.com/*
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js
 // @grant        GM_addStyle
 // ==/UserScript==
-GM_addStyle(".btn-group button{background-color:#4caf50;border:1px solid green;color:#fff;padding:10px 24px;cursor:pointer;float:left;position:sticky;position:-webkit-sticky;bottom:0}.btn-group:after{content:'';clear:both;display:table}.btn-group button:not(:last-child){border-right:none}.btn-group button:hover{background-color:#3e8e41}");
+GM_addStyle(".btn-group button{background-color:#4caf50;border:1px solid green;color:#fff;padding:10px 24px;cursor:pointer;float:left;position:fixed;bottom:0;left:0;right:0;height:50px;width:100%}.btn-group:after{content:'';clear:both;display:table}.btn-group button:not(:last-child){border-right:none}.btn-group button:hover{background-color:#3e8e41}");
 var $ = window.jQuery
 $(document).ready(function() {
     'use strict';
