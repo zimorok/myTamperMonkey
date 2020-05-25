422d58f9d4485661cb835c78bdae549ae2707b7a
zimorok <xsqade@gmail.com>
2020-05-25 13:46:03 +0800

    Update easyEYE.js
    fixed CSS

--- easyEYE.js
+++ easyEYE.js
@@ -2,14 +2,14 @@
 // @name         easyEye
 // @namespace    http://tampermonkey.net/
 // @updateURL	 https://github.com/zimorok/myTamperMonkey/raw/master/easyEYE.js
-// @version      0.1.6
+// @version      0.1.7
 // @description  Read your web novel easily on mobile with paginated content
 // @author       Zimorok
 // @match        *://boxnovel.com/*
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js
 // @grant        GM_addStyle
 // ==/UserScript==
-GM_addStyle(".btn-group button{background-color:#4caf50;border:1px solid green;color:#fff;padding:10px 24px;cursor:pointer;float:left;position:fixed;bottom:0;left:0;right:0;height:50px;width:100%;display:inline-block}.btn-group:after{content:'';clear:both;display:table}.btn-group button:not(:last-child){border-right:none}.btn-group button:hover{background-color:#3e8e41}");
+GM_addStyle(".btn-group a{background-color:#4caf50;border:1px solid green;color:#fff;padding:5px 15px;cursor:pointer;float:left;position:fixed;bottom:0;left:0;right:0;display:inline-block}.btn-group:after{content:'';clear:both;display:table}.btn-group a:not(:last-child){border-right:none}.btn-group a:hover{background-color:#3e8e41}");
 var $ = window.jQuery
 $(document).ready(function() {
     'use strict';
@@ -52,7 +52,7 @@ function paginate() {
 
 	//--creating next/previous button
 	//--append the next/previous button to <body>
-	var nav = '<div class="btn-group"><button href="#prev">&laquo; Prev</button><button href="#next">Next &raquo;</button></div>';
+	var nav = '<div class="btn-group"><a href="#prev">&laquo; Prev</a><a href="#next">Next &raquo;</a></div>';
 	$('#story').append(nav);
 
 	//--simple numbering and scroll-to 
